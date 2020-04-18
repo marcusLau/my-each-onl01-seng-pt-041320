2bdbc19847f47fbca45b94e4019b52974dfd04d6
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 20:55:24 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 # instantiate iterator 
+  while i < array.length
+    yield(array[i]) # pass element of collection to block 
+    i += 1
+  end
+  array
 end
