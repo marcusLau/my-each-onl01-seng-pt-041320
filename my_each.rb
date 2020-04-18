61d877376d63f2e539be200e45c38a8936e10194
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 20:55:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 def my_each(array)
-  i = 0
+  i = 0 # instantiate iterator 
   while i < array.length
-    yield(array[i])
+    yield(array[i]) # pass element of collection to block 
     i += 1
   end
   array
